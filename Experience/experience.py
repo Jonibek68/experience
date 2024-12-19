d815da920918bf085e0bb490a72d14ed83907fd9
Jonibek68 <jmuzaffarov14@gmail.com>
2024-12-19 15:05:39 +0500

    first commit

---
+++ Experience/experience.py
@@ -0,0 +1,8 @@
+print('Hello world')
+
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/Jonibek68/experience.git
+# git push -u origin main
